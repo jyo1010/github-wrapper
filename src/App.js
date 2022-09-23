3cf29342081eeed0fe95e77730a7e486b806b91c
jyothika <jyothika1010@gmail.com>
2022-09-24 02:05:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [username,setUsername]=useState('');
+  const [resData,setResData]=useState('');
+
+
+  const handleUsernameInput=(event)=>{
+      setUsername(event.target.value);
+  }
+
+  const handleSubmit=(event) => {
+    event.preventDefault();
+    console.log(username);
+    fetch('https://api.github.com/users/'+ username)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setResData(data);
+    });
+
+    setUsername('');
+
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+
+        <h1 className='heading'>Git hub profile finder 🔎</h1>
+        <form onSubmit={handleSubmit} className='formCard' >
+          <input type="text" placeholder='github ID name' onChange={handleUsernameInput} value={username}/>
+          <button>Submit</button>
+        </form>
+       
+
+        {resData.message}
+        {resData && (resData.message!='Not found')&&(
+            <div className='userDetailCard'>
+
+             
+              <div className='userDetailBody'>
+                   <p className='name'>{resData.name}</p>
+                   <em className='username'> User Id: {resData.login}</em>
+
+                  <div className='follow'>
+                    <p>Followers 👥: {resData.followers}</p>
+                    <p>Following 👥: {resData.following}</p>
+                    
+                  </div>
+
+                  <div className='profDetail'>
+                    <p>💼 {resData.company}</p>
+                    <p>📋{resData.bio}</p>
+                    <p>📑 Public repositoties:{resData.public_repos}</p>
+
+                    
+                  </div>
+              </div> 
+
+              <div className='userImage'>     
+                 <img  src={resData.avatar_url}></img>
+              </div>  
+             
+
+              
+            </div>
+
+
+
+          )
+        }
+     
+      </div>
+ 
+    </>
   );
+    
 }
 
 export default App;
